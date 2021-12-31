d749111cdd7733804b4b59c72f9d630917a27e9e
HanMinSS <gksals6464@naver.com>
2022-01-01 01:00:27 +0900

    ✅ 라우트 연결

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# apple-store-clone
- 프로젝트라이언 애플스토어 클론코딩
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* History api를 이용하여 라우팅 처리를 할 수 있게 해줍니다. */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
